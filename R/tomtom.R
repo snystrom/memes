e63f3eb66bebde12b6ec972f2619f48f1fefd5fc
snystrom <snystrom@ad.unc.edu>
2020-04-13 19:02:54 -0400

    working on tomtom import from xml
    Former-commit-id: a4bdb37f28b1dd7b19a810897138e7a103173108

--- R/tomtom.R
+++ R/tomtom.R
@@ -4,16 +4,21 @@ handle_tomtom_database_path <- function(path = NULL){
   f(path = path)
 }
 
-
 runTomTom <- function(input, database = NULL,
                       outdir = paste0(dirname(input), "/tomtom"),
+                      thresh = 10,
+                      min_overlap = 5,
+                      dist = "pearson",
+                      evalue = T,
                       meme_path = NULL, ...){
+  # use TOMTOM server default values which differ from commandline defaults
+  # TODO: email TOMTOM maintainers to ask if this is really a better default?
 
-  command <- handle_meme_path(meme_path = meme_path, util = "tomtom")
+  command <- handle_meme_path(path = meme_path, util = "tomtom")
 
   database <- handle_tomtom_database_path(path = database)
 
-  flags <- prepareTomTomFlags(...)
+  flags <- prepareTomTomFlags(outdir = outdir, thresh = thresh, min_overlap = min_overlap, dist = dist, evalue = evalue, ...)
   flags <- c(flags, input, database)
 
   processx::run(command, flags, spinner = T)
@@ -26,13 +31,52 @@ runTomTom <- function(input, database = NULL,
   return(tomtom_out)
 }
 
-prepareTomTomFlags <- function(input, database, ...){
+prepareTomTomFlags <- function(outdir, thresh, min_overlap, dist, evalue, ...){
   #TODO:
   # lookup table converting arguments with - to _ so
   # user doesn't have to escape flags
+  argsDict = c("min_overlap" = "min-overlap",
+               "outdir" = "oc")
+
   flags <- dotargs::getAllArgs() %>%
-    dotargs::argsToFlags() %>%
+    dotargs::argsToFlags(argsDict) %>%
     dotargs::crystallize_flags()
 
   return(flags)
+}
+
+
+readTomTom_txt <- function(txt){
+  df <- readr::read_tsv(txt, col_names = c("query_id",
+                                           "target_id",
+                                           "optimal_offset",
+                                           "pvalue",
+                                           "evalue",
+                                           "qvalue",
+                                           "overlap",
+                                           "query_consensus",
+                                           "target_consensus",
+                                           "orientation"),
+                        skip = 1,
+                        comment = "#",
+                        col_types =
+                        readr::cols(
+                          query_id = readr::col_character(),
+                          target_id = readr::col_character(),
+                          optimal_offset = readr::col_double(),
+                          pvalue = readr::col_double(),
+                          evalue = readr::col_double(),
+                          qvalue = readr::col_double(),
+                          overlap = readr::col_integer(),
+                          query_consensus = readr::col_character(),
+                          target_consensus = readr::col_character(),
+                          orientation = readr::col_character()
+                        ))
+
+  names(df) <- names(df) %>%
+    gsub(" ", "_", .) %>%
+    gsub("-", "", .) %>%
+    tolower
+
+  return(df)
 }
