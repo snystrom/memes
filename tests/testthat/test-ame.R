e2206aa099113b93c95bf3f3558347de136fbeef
snystrom <snystrom@ad.unc.edu>
2020-04-23 17:59:05 -0400

    adding some unfinished ame unit tests
    Former-commit-id: 7d430b044479e8f12adde64c3b9c5279d30f8241

---
+++ tests/testthat/test-ame.R
@@ -0,0 +1,13 @@
+testthat("AME Internal functions work",{
+  expect_equal(prepareAmeFlags("shuffle", "outdir"), c("--control", "--shuffle--", "--oc", "outdir"))
+  expect_equal(prepareAmeFlags("test.fa", "outdir"), c("--control", "test.fa", "--oc", "outdir"))
+  expect_equal(prepareAmeFlags(NULL, "outdir"), c("--oc", "outdir"))
+  expect_equal(prepareAmeFlags(NA, "outdir"), c("--oc", "outdir"))
+})
+
+# expect error
+runAme("inst/extdata/fasta_ex/fa1.fa", NULL)
+# expect success, partitioning mode, fasta requires fasta scores
+runAme("inst/extdata/fasta_ex/fa1.fa", NA)
+# expect success, discriminative mode
+runAme("inst/extdata/fasta_ex/fa1.fa", "inst/extdata/fasta_ex/fa2.fa")
