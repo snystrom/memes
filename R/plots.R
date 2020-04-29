82d2aae10d4351650e8affe9ca8f467a8e456b7c
snystrom <snystrom@ad.unc.edu>
2020-04-29 17:32:03 -0400

    updating view_tomtom motifs to indicate a motif had no matches.
    Former-commit-id: 205bbed1f3b3ffb04d517d287a065455f78066e9

--- R/plots.R
+++ R/plots.R
@@ -1,3 +1,19 @@
+#' Add title to complex plot with cowplot
+#'
+#' @param plot plot object to add title to
+#' @param title text of title
+#' @param ... passed to [cowplot::draw_text()]
+#'
+#' @return
+#'
+#' @noRd
+#'
+cowplot_title <- function(plot, title, ...){
+  title <- cowplot::ggdraw() +
+    cowplot::draw_text(title, ...)
+  cowplot::plot_grid(plotlist = list(title, plot), ncol = 1, rel_heights = c(0.1, 1))
+}
+
 #' Compare top tomtom hits to original motif
 #'
 #' Although TomTom does a good job of matching unknown motifs to known motifs,
@@ -5,10 +21,18 @@
 #' manually inspect the hits. This function provides a quick utility to compare
 #' matches.
 #'
+#' This is intended to be a function used interactively and may not always be
+#' the best tool for creating publication-quality figures. Results with matches
+#' return ggseqlogo outputs which can be further manipulated using
+#' [ggplot2::theme()] calls, but results containing no matches are static plots.
+#'
 #' @param results results data.frame from runTomTom
 #' @param top_n number of matched motifs to return in plot (default: "all")
+#' @param ... passed to [universalmotif::view_motifs()]
 #'
-#' @return plot of input motif vs the top n number of tomtom matched motifs
+#' @return plot of input motif vs the top n number of tomtom matched motifs. If
+#'   no match found, will plot "No Match". Note: the "No Match" plots are not
+#'   amenable to ggplot theme() manipulations, while all others are.
 #' @export
 #'
 #' @examples
@@ -18,9 +42,13 @@
 #' view_tomtom_hits(results, top_n = 3)
 #'
 #' }
-view_tomtom_hits <- function(results, top_n = "all"){
-
+view_tomtom_hits <- function(results, top_n = "all", ...){
+  # TODO: if tomtom is empty, return NONE as plot below??
   purrr::map2(results$motif, results$tomtom, ~{
+
+    if (is.null(.y)) {
+      return(view_tomtom_nomatch(.x, ...))
+    }
 
     if (top_n == "all") {select <- 1:length(.y$match_motif)}
     else if (is.numeric(top_n)) {select <- 1:top_n}
@@ -31,6 +59,27 @@ view_tomtom_hits <- function(results, top_n = "all"){
     motifList <- c(list(.x), .y$match_motif[select]) %>%
       purrr::discard(is.null)
 
-    universalmotif::view_motifs(motifList)
+    universalmotif::view_motifs(motifList, ...)
   })
 }
+
+#' Plot motif with "No Match" below.
+#'
+#' NOTE: this doesn't do any checking, and is not meant to be called directly by
+#' user. Used internally in [view_tomtom_hits()]
+#'
+#' @param motif universalmotif
+#' @param ... passed to [universalmotif::view_motifs()]
+#'
+#' @return
+#'
+#' @noRd
+view_tomtom_nomatch <- function(motif, ...){
+  plot <- universalmotif::view_motifs(motif, ...) %>%
+    cowplot_title(motif@name, hjust = 0.25, size = 11)
+  noMatch <- cowplot::ggdraw() +
+    cowplot::draw_text("No Match", size = 100)
+
+  cowplot::plot_grid(plot, noMatch, ncol = 1)
+}
+
