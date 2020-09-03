2c48918f844f184d46dbe8fd926bf594abcc6179
snystrom <snystrom@ad.unc.edu>
2020-09-03 19:29:43 -0400

    remove MEME utils test file (moved to dreme)
    Former-commit-id: 8d8b5d59f4d7952be746ab4a6fab9dfdc0ec4445

--- tests/testthat/test-MEME_Utils.R
+++
@@ -1,7 +0,0 @@
-test_that("Dreme flags correctly parsed", ~{
-  expect_equal(prepareDremeFlags(input = "input.fa", control = "shuffle"),
-               c("-p", "input.fa"))
-  expect_equal(prepareDremeFlags(input = "input.fa", control = "background.fa"),
-               c("-p", "input.fa", "-n", "background.fa"))
-})
-
