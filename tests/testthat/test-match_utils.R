38f8a01112d7e858107c1023f0c1a0d3d6be2fec
snystrom <snystrom@ad.unc.edu>
2020-09-03 19:27:20 -0400

    finish match utils tests
    Former-commit-id: abda6489af8359e5892ea4e60e3a5be2f99f907f

--- tests/testthat/test-match_utils.R
+++ tests/testthat/test-match_utils.R
@@ -1,17 +1,20 @@
-data("example_tomtom")
 
-# test force_best_match
-expect_error(
-  force_best_match(example_tomtom, c("bad"= "bad")),
-  "invalid names: bad"
-)
+test_that("match utils work", {
+  data("example_tomtom", package = "memes")
+  # test force_best_match
+  expect_error(
+    force_best_match(example_tomtom, c("bad"= "bad")),
+    "invalid names: bad"
+  )
 
-expect_error(
-  force_best_match(example_tomtom, c("example_motif"= "bad")),
-  "bad is not found within "
-)
+  expect_error(
+    force_best_match(example_tomtom, c("example_motif"= "bad")),
+    "bad is not found within "
+  )
 
-expect_equal(
-  force_best_match(example_tomtom, c("example_motif"= "Lag1_Cell"))$best_match_name,
-  "Lag1_Cell"
-)
+  expect_equal(
+    force_best_match(example_tomtom, c("example_motif"= "Lag1_Cell"))$best_match_name,
+    "Lag1_Cell"
+  )
+
+})
