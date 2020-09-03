99990a97c1e411ac3464ad47d0376c43bd5febe0
snystrom <snystrom@ad.unc.edu>
2020-09-03 19:27:36 -0400

    add flag parse check to runDreme test
    Former-commit-id: 98d6ed84a8e02111145f155ea0313f0f0e973903

--- tests/testthat/test-dreme.R
+++ tests/testthat/test-dreme.R
@@ -22,6 +22,13 @@ test_that("runDreme works", {
 
 })
 
+test_that("Dreme flags correctly parsed", ~{
+  expect_equal(prepareDremeFlags(input = "input.fa", control = "shuffle"),
+               c("-p", "input.fa"))
+  expect_equal(prepareDremeFlags(input = "input.fa", control = "background.fa"),
+               c("-p", "input.fa", "-n", "background.fa"))
+})
+
 test_that("dreme_results & universalmotif list validators work", {
   # Test dreme_results validators
   spec <- new_dreme_results()
