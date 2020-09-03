31766624bda48ede1d1e0087490b8545656be8b6
snystrom <snystrom@ad.unc.edu>
2020-09-03 19:24:17 -0400

    add input method tests
    Former-commit-id: 8957d0bc7d79bc4a053ea3f6e7edf025960e0aa4

--- tests/testthat/test-input_methods.R
+++ tests/testthat/test-input_methods.R
@@ -2,42 +2,46 @@ context("input methods & meme_database_path search_function")
 
 #####
 # MOVE THIS TO HELPER
-fa <- memes:::duplicate_file("inst/extdata/fasta_ex/fa1.fa")
-dreme_out <- runDreme(fa, "shuffle", e = 39)
+#fa <- system.file("extdata/fasta_ex/fa1.fa", package = "memes") %>%
+#  Biostrings::readDNAStringSet()
+#dreme_out <- runDreme(fa, "shuffle", e = 39)
+dreme_out <- system.file("extdata/dreme_example/dreme.xml", package = "memes") %>%
+  importDremeXML()
+
+fb <- system.file("extdata/flyFactorSurvey_cleaned.meme", package = "memes")
+fb_name <- basename(fb)
 ####
 
 test_that("database works with user-input settings", {
-  fb <- "inst/extdata/db/fly_factor_survey_id.meme"
-  expect_equal(basename(handle_meme_database_path(fb)), basename(fb))
+  expect_equal(basename(search_meme_database_path(fb)), basename(fb))
 
   path_motif_named <- c(fb, "motif" = universalmotif::create_motif())
-  expect_equal(basename(handle_meme_database_path(path_motif_named)),
+  expect_equal(basename(search_meme_database_path(path_motif_named)),
                c(basename(fb), "motif"))
 
   noNames <- c(fb, universalmotif::create_motif())
-  expect_equal(basename(handle_meme_database_path(noNames)),
-               c("fly_factor_survey_id.meme", "2"))
+  expect_equal(basename(search_meme_database_path(noNames)),
+               c(fb_name, "2"))
 
   both_named <- c('flyfactor' = fb, "motif" = universalmotif::create_motif())
-  expect_equal(basename(handle_meme_database_path(both_named)),
+  expect_equal(basename(search_meme_database_path(both_named)),
                c("flyfactor", "motif"))
 })
 
 test_that("dreme-res works as database",{
   # uses name
-  expect_equal(handle_meme_database_path(list("test" = dreme_out)) %>% basename, "test")
+  expect_equal(search_meme_database_path(list("test" = dreme_out)) %>% basename, "test")
   # uses order as default name
-  expect_equal(handle_meme_database_path(list(dreme_out)) %>% basename, "1")
+  expect_equal(search_meme_database_path(list(dreme_out)) %>% basename, "1")
 })
 
 test_that("Type checking errors correctly", {
-  expect_error(handle_meme_database_path(dreme_out), "data.frame")
-  expect_error(handle_meme_database_path(2), "path must be")
-  expect_error(handle_meme_database_path(factor(1,2,3)), "path must be")
+  expect_error(search_meme_database_path(dreme_out), "data.frame")
+  expect_error(search_meme_database_path(2), "path must be")
+  expect_error(search_meme_database_path(factor(1,2,3)), "path must be")
 })
 
 test_that("motif input method dispatch works",{
-  fb <- "inst/extdata/db/fly_factor_survey_id.meme"
   # character(1)
   expect_equal(motif_input(fb), list(metadata = NULL, path = fb))
   expect_error(motif_input(c(fb, fb)), "length == 1")
