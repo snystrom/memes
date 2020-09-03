7500878c137965bcfb699c842c2a96eb5a6bbb4e
snystrom <snystrom@ad.unc.edu>
2020-09-03 19:18:30 -0400

    cleaned up dreme tests -- many of these are probably ... unneeded...
    Former-commit-id: a8032d18bdd4c40b1ad5b8e616a1324d08a083e5

--- tests/testthat/test-dreme.R
+++ tests/testthat/test-dreme.R
@@ -1,89 +1,90 @@
-skip_if(T, "Only works with dreme install")
+skip_if_not(meme_is_installed(), "Only works with dreme install")
 
-fa <- duplicate_file("inst/extdata/fasta_ex/fa1.fa")
-fa2 <- duplicate_file("inst/extdata/fasta_ex/fa2.fa")
-
-# won't detect @ normal threshold
-expect_null(runDreme(fa, "shuffle"))
-expect_true(is_dreme_results(runDreme(fa, "shuffle", e = 39)))
+# Setup
+fa <- system.file("extdata/fasta_ex/fa1.fa", package = "memes") %>%
+  Biostrings::readDNAStringSet()
+fa2 <- system.file("extdata/fasta_ex/fa2.fa", package = "memes") %>%
+  Biostrings::readDNAStringSet()
 
 dreme_out <- runDreme(fa, "shuffle", e = 39)
 
-# test dreme catch error
-empty_fa <- tempfile()
-file.create(empty_fa)
-expect_error(suppressMessages(runDreme(empty_fa, "shuffle"), "No sequences"))
+test_that("runDreme works", {
+  # won't detect @ normal threshold
+  expect_null(runDreme(fa, "shuffle"))
 
-# test universalmotif validators
-expect_true(is_universalmotif_list(list(universalmotif::create_motif())))
-expect_false(is_universalmotif_list(list(universalmotif::create_motif(), "no")))
+  expect_true(is_dreme_results(dreme_out))
 
-# Test dreme_results validators
-spec <- new_dreme_results()
-bad <- spec[,1:5]
+  # test dreme catch error if fasta empty
+  empty_fa <- tempfile()
+  file.create(empty_fa)
+  expect_error(suppressMessages(runDreme(empty_fa, "shuffle"), "No sequences"))
+  file.remove(empty_fa)
 
-expect_true(is_dreme_results(dreme_out))
-expect_false(is_dreme_results(bad))
+})
 
-expect_null(error_dreme_results(dreme_out))
-expect_error(error_dreme_results(spec), "motif column is empty")
-expect_error(error_dreme_results(bad), "Missing columns")
+test_that("dreme_results & universalmotif list validators work", {
+  # Test dreme_results validators
+  spec <- new_dreme_results()
+  bad <- spec[,1:5]
 
-#####
-# test multiple dispatch
-peaks <- "inst/extdata/peaks/peaks.tsv" %>%
-  readr::read_tsv() %>%
-  GRanges
-dm.genome <- BSgenome.Dmelanogaster.UCSC.dm3::BSgenome.Dmelanogaster.UCSC.dm3
+  expect_true(is_dreme_results(dreme_out))
+  expect_false(is_dreme_results(bad))
 
-seq <- get_sequence(peaks, dm.genome)
-fa <- write_fasta(seq)
+  expect_null(error_dreme_results(dreme_out))
+  expect_error(error_dreme_results(spec), "motif column is empty")
+  expect_error(error_dreme_results(bad), "Missing columns")
+
+  # test universalmotif validators
+  expect_true(is_universalmotif_list(list(universalmotif::create_motif())))
+  expect_false(is_universalmotif_list(list(universalmotif::create_motif(), "no")))
+})
+
+test_that("runDreme dispatch works", {
+  # test multiple dispatch
+  peaks <- system.file("extdata/peaks/peaks.tsv", package = "memes") %>%
+    readr::read_tsv(col_types = c("seqnames" = "c",
+                                  "start" = "i",
+                                  "end" = "i")) %>%
+    GenomicRanges::GRanges()
+  dm.genome <- BSgenome.Dmelanogaster.UCSC.dm3::BSgenome.Dmelanogaster.UCSC.dm3
+
+  seq <- get_sequence(peaks, dm.genome)
+  seq <- get_sequence(peaks, dm.genome)
+  fa <- write_fasta(seq)
+
+  ## test fasta path input
+  expect_equal(runDreme(fa, "shuffle", e = 49), runDreme(seq, "shuffle", e = 49))
+
+  ## test stringset list input
+  seq_by_type <- peaks %>%
+    data.frame %>%
+    dplyr::mutate(type = c(rep("A", nrow(.) / 2), rep("B", nrow(.) / 2))) %>%
+    GenomicRanges::GRanges() %>%
+    split(mcols(.)$type) %>%
+    # genome is dm3
+    get_sequence(dm.genome)
+
+  # Use all input w/ shuffled background sequence
+  expect_named(runDreme(seq_by_type, "shuffle", e = 70), c("A", "B"))
+  # use "A" as background
+  expect_named(runDreme(seq_by_type, "A", e = 70), "B")
+  # Use invalid background
+  expect_error(runDreme(seq_by_type, "d", e = 70), "d was not found")
+
+  # test that control names are all inside names of input
+  # ie that if name isn't in name, throws error
+  expect_error(runDreme(seq_by_type, c("B", "C"), e = 70), "names passed to control do not exist")
+
+})
+
+test_that("runDreme error checking works", {
+  ## test error checking
+  expect_error(suppressMessages(runDreme(fa, "shuffle", et = 39)),
+               "\"e\" instead of", class = "error")
+  ## Ensure error catch works with list input also
+  expect_error(suppressMessages(runDreme(seq_by_type, "A", et = 70)),
+               "\"e\" instead of", class = "error")
 
 
-## test fasta path input
-runDreme(fa, "shuffle", e = 49)
-runDreme(seq, "shuffle", e = 49)
+})
 
-runDreme(fa, seq)
-runDreme(fa, fa)
-
-## test stringset list input
-seq_by_type <- peaks %>%
-  data.frame %>%
-  dplyr::mutate(type = c(rep("A", nrow(.) / 2), rep("B", nrow(.) / 2))) %>%
-  GRanges %>%
-  split(mcols(.)$type) %>%
-  # genome is dm3
-  get_sequence(dm.genome)
-
-# Use all input w/ shuffled background sequence
-expect_named(runDreme(seq_by_type, "shuffle", e = 70), c("A", "B"))
-# use "A" as background
-expect_named(runDreme(seq_by_type, "A", e = 70), "B")
-# Use invalid background
-expect_error(runDreme(seq_by_type, "d", e = 70), "d was not found")
-
-# test that control names are all inside names of input
-# ie that if name isn't in name, throws error
-error('need test here')
-
-
-## test error checking
-fa <- memes:::duplicate_file("inst/extdata/fasta_ex/fa1.fa")
-expect_error(suppressMessages(runDreme(fa, "shuffle", et = 39)),
-             "\"e\" instead of", class = "error")
-## Ensure error catch works with list input also
-expect_error(suppressMessages(runDreme(seq_by_type, "A", et = 70)),
-             "\"e\" instead of", class = "error")
-
-
-###
-#test tomtom
-
-# dev tt_result_parse
-# want data.frame w/ results & motif_match col w/ universalmotif object
-
-#tomtom_xml_path <- tt_files$xml
-#####
-stop("dev below")
-runDreme(fa, fa2)
