7fc3b31aeb946958bc666fd1c3d049a639ddf20f
snystrom <snystrom@ad.unc.edu>
2020-09-02 21:59:14 -0400

    updated fimo unit tests
    Former-commit-id: 2f93179963754744fbca92296083cd8a96627716

--- tests/testthat/test-fimo.R
+++ tests/testthat/test-fimo.R
@@ -1,50 +1,41 @@
-ffdb <- universalmotif::read_meme("inst/extdata/db/fly_factor_survey_id.meme")
-df <- memes:::universalmotif_to_meme_df(ffdb)
-df %<>%
-  dplyr::filter(grepl("Eip93F", id))
-e93_motif <- ffdb %>%
-  universalmotif::filter_motifs(name = df$id)
+skip_if_not(meme_is_installed())
 
-universalmotif::write_meme(e93_motif, "inst/extdata/db/e93_motif.meme")
+test_that("runFimo works", {
+  peaks <- system.file("extdata/peaks/peaks.tsv", package = "memes") %>%
+    readr::read_tsv(col_types = c("seqnames" = "c", "start" = "i", "end" = "i")) %>%
+    GenomicRanges::GRanges()
 
-suppressPackageStartupMessages(library(GenomicRanges))
+  dm.genome <- BSgenome.Dmelanogaster.UCSC.dm6::BSgenome.Dmelanogaster.UCSC.dm6
 
-peaks <- "inst/extdata/peaks/peaks.tsv" %>%
-  readr::read_tsv() %>%
-  GRanges
+  e93_motif <- universalmotif::create_motif("CCRAAAW", name = "E93", altname = "Eip93F")
 
+  regions <- peaks %>%
+    GenomicRanges::resize(200, fix = "center")
 
+  fimo_res <- regions %>%
+    get_sequence(dm.genome) %>%
+    runFimo(e93_motif, thresh = 1e-3, skip_matched_sequence = FALSE) %>%
+    add_sequence(dm.genome)
 
-e93_motif <- universalmotif::read_meme("inst/extdata/db/e93_motif.meme")
-dm.genome <- BSgenome.Dmelanogaster.UCSC.dm6::BSgenome.Dmelanogaster.UCSC.dm6
+  expect_s4_class(fimo_res, "GenomicRanges")
 
-fimo_res <- peaks %>%
-  resize(200, fix = "center") %>%
-  get_sequence(dm.genome) %>%
-  runFimo(e93_motif)
+  # Ensure add_sequence matches the detected sequences
+  fimo_res %<>% add_sequence(dm.genome)
+  expect_equal(fimo_res$matched_sequence, fimo_res$sequence)
 
-fimo_res$stdout[1] %>%
-  readr::write_lines("fimo_test.tsv", sep = "")
+  # errror no names in header
+  seq <- universalmotif::create_sequences()
+  motif <- universalmotif::create_motif()
+  expect_message(try(runFimo(seq, motif, parse_genomic_coord = FALSE), silent = TRUE), "header")
+  expect_error(suppressMessages(runFimo(seq, motif, parse_genomic_coord = FALSE)))
 
-#motif_input(e93_motif) -> x
+  # run, but message "no matches detected"
+  names(seq) <- seq_along(seq)
+  expect_null(suppressMessages(runFimo(seq, motif, thresh = 1e-10)))
+  expect_message(runFimo(seq, motif, thresh = 1e-10), "No matches were detected")
 
-readr::read_tsv(fimo_res$stdout) %>%
-  dplyr::rename_all(~{gsub("-", "", .)}) %>%
-  dplyr::rename("seqnames" = "sequence_name") %>%
-  GRanges
+  # Suggest psp instead of psspp
+  expect_error(suppressMessages(runFimo(seq, motif, psspp = 'x')), class = "usethis_error", regexp = "Invalid flags")
+  expect_message(try(runFimo(seq, motif, psspp = 'x'), silent = TRUE), "Error processing command line options")
 
-
-####
-
-# errror no names in header
-seq <- universalmotif::create_sequences()
-motif <- universalmotif::create_motif()
-runFimo(seq, motif, parse_genomic_coord = FALSE)
-
-# run, but message "no matches detected"
-names(seq) <- seq_along(seq)
-runFimo(seq, motif, parse_genomic_coord = FALSE)
-runFimo(seq, motif)
-
-# Suggest psp instead of psspp
-runFimo(seq, motif, psspp = 'x')
+})
