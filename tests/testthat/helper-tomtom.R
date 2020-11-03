d659ad75ed686e3755e7bb40bf73aee4c49052da
snystrom <snystrom@users.noreply.github.com>
2020-11-03 12:13:22 -0500

    Version bump.
    Former-commit-id: a9695d245bb8c4d261f5641c5bb73f60529b66e2

--- tests/testthat/helper-tomtom.R
+++ tests/testthat/helper-tomtom.R
@@ -4,5 +4,5 @@ fa <- system.file("extdata/fasta_ex/fa1.fa", package = "memes")
 dreme_out <- runDreme(fa, "shuffle", e = 39, outdir = tempdir())
 
 fa2 <- universalmotif::create_sequences(seqnum = 2, seqlen = 100, rng.seed = 123)
-names(fa2) <- 1:length(fa2)
+names(fa2) <- seq_len(fa2)
 meme_out <- runMeme(fa2, parse_genomic_coord = F)
