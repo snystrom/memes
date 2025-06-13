2df1734083014d3cb92de5e7fb3bbdd5ba130c14
Spencer <snystrom@users.noreply.github.com>
2025-06-13 19:10:57 -0400

    Cleanup process error check.
    This file printed too much stuff to stdout during testthat. Also ran
    styler hence the larger diff.

--- tests/testthat/test-process_error_check.R
+++ tests/testthat/test-process_error_check.R
@@ -1,18 +1,28 @@
 skip_if_not(meme_is_installed())
 a <- search_meme_path(util = "ame")
 
-a_out <- list(status = 1,
-              stdout = "output\n",
-              stderr = "Some error")
+a_out <- list(
+  status = 1,
+  stdout = "output\n",
+  stderr = "Some error"
+)
 
 
 test_that("process_check_error suggests correctly", {
   # No user flags should not check errors
   expect_error(
-    suppressMessages(
-      process_check_error(a_out, help_fun = ~{ame_help(a)},
-      flags_fun = ~{gsub("-", "_", .x)})
-      ),
+    capture.output(
+      suppressMessages(
+        process_check_error(a_out,
+          help_fun = ~ {
+            ame_help(a)
+          },
+          flags_fun = ~ {
+            gsub("-", "_", .x)
+          }
+        )
+      )
+    ),
     "Some error",
     class = "error"
   )
@@ -20,24 +30,41 @@ test_that("process_check_error suggests correctly", {
   # lambda functions should work as input
   # user flags w/typo should get suggested
   expect_error(
-    process_check_error(a_out,
-                        help_fun = ~{processx::run(a, "-h", error_on_status = F)$stderr},
-                        user_flags = c("evalue_reorpt_threshold"),
-                        flags_fun = ~{gsub("-", "_", .x)}, default_help_fun = TRUE),
+    capture.output(
+      suppressMessages(
+        process_check_error(a_out,
+          help_fun = ~ {
+            processx::run(a, "-h", error_on_status = F)$stderr
+          },
+          user_flags = c("evalue_reorpt_threshold"),
+          flags_fun = ~ {
+            gsub("-", "_", .x)
+          }, default_help_fun = TRUE
+        )
+      )
+    ),
     "\"evalue_report_threshold\" instead of: \"evalue_reorpt_threshold\"",
     class = "error"
   )
 
   # Also ensure usethis error is returned above
   expect_error(
-    process_check_error(a_out,
-                        help_fun = ~{processx::run(a, "-h", error_on_status = F)$stderr},
-                        user_flags = c("evalue_reorpt_threshold"),
-                        flags_fun = ~{gsub("-", "_", .x)}, default_help_fun = TRUE),
+    suppressMessages(
+      capture.output(
+        process_check_error(a_out,
+          help_fun = ~ {
+            processx::run(a, "-h", error_on_status = F)$stderr
+          },
+          user_flags = c("evalue_reorpt_threshold"),
+          flags_fun = ~ {
+            gsub("-", "_", .x)
+          }, default_help_fun = TRUE
+        )
+      )
+    ),
     "\"evalue_report_threshold\" instead of: \"evalue_reorpt_threshold\"",
     class = "usethis_error"
   )
-
 })
 
 # TODO:
